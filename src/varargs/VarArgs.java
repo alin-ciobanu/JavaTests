4968117179491b285329c03a3a17bcd5a8117ef0
Alin <alin.ciobanu@cti.pub.ro>
2013-12-20 10:27:34 +0200

    VarArgs

--- src/varargs/VarArgs.java
+++ src/varargs/VarArgs.java
@@ -1,5 +1,28 @@
 package varargs;
 
+import java.math.BigDecimal;
+
 public class VarArgs {
+
+	public static BigDecimal sum (BigDecimal... args) {
+
+		BigDecimal sum = new BigDecimal(0);
+
+		for (BigDecimal current : args) {
+			sum = sum.add(current);
+		}
+
+		return sum;
+
+	}
+
+	public static void main(String[] args) {
+
+		BigDecimal sum = VarArgs.sum(new BigDecimal(1), new BigDecimal(2), new BigDecimal(3), new BigDecimal(4),
+				new BigDecimal(5), new BigDecimal(6), new BigDecimal(7), new BigDecimal(8));
+
+		System.out.println(sum);
+
+	}
 
 }
