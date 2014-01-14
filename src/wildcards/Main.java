e1550c7755d54db46792b29b33fa2ba2e569fc23
Alin <alin.ciobanu@cti.pub.ro>
2014-01-14 14:52:27 +0200

    wildcards updated

--- src/wildcards/Main.java
+++ src/wildcards/Main.java
@@ -7,7 +7,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		List<Object> list = new ArrayList<Object>();
+		List<Animal> animalList = new ArrayList<Animal>();
+		List<SpecialDog> specialDogList = new ArrayList<SpecialDog>();
+
+		Testing.add(animalList);
+
+		// this will result in a compile error:
+		// Testing.add(specialDogList); // compile error
+		// you need to pass a List<? super Dog>
 
 	}
 
@@ -17,7 +24,21 @@ public class Main {
 class Testing {
 
 	public static void add (List<? super Dog> list) {
-		list.add(new Dog()); // only dogs can be added in this list
+		list.add(new Dog());
+		list.add(new SpecialDog());
+		// only dogs and subtypes of dog can be added in this list
+
+		// don't try these:
+		// list.add(new Object()); // compile error
+		/*
+		list.add(new AbstractAnimal() {
+
+			@Override
+			public void makeSound() {
+				System.out.println("MOO");
+			}
+		}); */// compile error - AbstractAnimal is a supertype of Dog, can't add
+
 	}
 
 }
---
+++ src/wildcards/SpecialDog.java
@@ -0,0 +1,5 @@
+package wildcards;
+
+public class SpecialDog extends Dog {
+
+}
